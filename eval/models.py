4be251d5e2b96cf6416c7bc9904c0f174bcb9e4f
Alexandru Tache <hadesgames@gmail.com>
2011-05-04 15:59:57 +0300

    Added some base models for the eval

--- eval/models.py
+++ eval/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Contest(models.Model):
+  
+  start_time = models.DateTimeField()
+  duration = models.IntegerField()
+
+class Problem(models.Model):
+
+  solution = models.CharField(max_length= 10)
+  
+  value = models.IntegerField(default = 20)
+  wrong = models.IntegerField(default = 0)
+  corect = models.IntegerField(default = 0)
+  
+  contest = models.ForeignKey(Contest)
+
+class Team(models.Model):
+
+  name = models.CharField(max_length=40)
+  score = models.IntegerField()
+  special_problem = models.ForeignKey(Problem)
+
+class Answer(models.Model):
+
+  time = models.DateTimeField()
+  team = models.ForeignKey(Team)
+  Problem = models.ForeignKey(Problem)
+
+
+
+
+  
+  

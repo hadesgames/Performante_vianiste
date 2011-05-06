1f18a918701868fd0699da90c360f72b51a1f730
Alexandru Tache <hadesgames@gmail.com>
2011-05-06 17:57:11 +0300

    The alpha version of the daemon + some more model changes.
    The daemon is absolutely untested. Have fun :D

--- daemon.py
+++ daemon.py
@@ -4,40 +4,73 @@ import settings
 setup_environ(settings)
 
 from  eval.models import *
-  
+mode = "DEBUG"
+
+bonus = [20, 15, 10, 8, 6, 5, 4, 3, 2, 1]
+solved_all_bonus = [100, 60, 40, 30, 20, 10]
+
 def do_work(answer):
   # validate that the contest is still running
+  # TO DO: bonus for when a team completes all the problems
   team = answer.team
   problem = answer.problem
   contest = problem.contest
   
-
-  if contest.start_time + datetime.timedelta(minutes = constest.duration) < datetime.now() :
-    answer.done = True
+  if contest.time_passed().minutes >= duration :
+    answer.status = 3
     answer.save()
     return
 
-  # setup coeficient for the special problem
+  #check if the the problem has already been answered by this team
 
-  if team.special_problem == problem:
-    multiplier = 2
-  else:
-    multiplier = 1
+  if team.answer_set.filter(status = 2. problem = problem).exists() :
+    answer.status = 3
+    answer.save()
+    return 
 
   if problem.solution == answer.solution :
     #good answer
-    pass
+    #TO DO : check that the same team doesn't summit the correct answer multiple times
+    answer.status = 2
+    
+    points_gained = problem.score()
+    try:
+      points_gained += bonus[problem.corect]
+    except IndexError:
+      pass
+
+    team.solved+=1
+    if team.solved == contest.problem_set.count():
+      try:
+        points_gained += solved_all_bonus[contest.fully_solved]
+      except IndexError:
+        pass
+      contest.fully_solved += 1
+      contest.save()
+    
+    # modify the problem
+    problem.value = problem.score()
+    problem.corect += 1
+    problem.save()
   else:
     #wrong answer
-    pass      
-
-  
-  
-  
+    answer.status = 1
+    
+    points_gained = -10
+    problem.value += problem.can_score_change() and 2
+    problem.save()
+    
+  if problem == team.special_problem:
+    points_gained *= 2
+    team.special_score += points_gained
+    
+  team.score += points_gained
+  team.save()
+  answer.save()
 
 def work():
   while 1:
-    answer_list = Answer.objects.filter(done = False)
+    answer_list = Answer.objects.filter(status = 0)
     for answer in answer_list:
       do_work(answer)
     break
--- eval/models.py
+++ eval/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
@@ -6,30 +7,53 @@ class Contest(models.Model):
   
   start_time = models.DateTimeField()
   duration = models.IntegerField()
+  fully_solved = models.IntegerField(default = 0)
+  def time_passed(self):
+    return datetime.datetime.now() - self.start_time
+  
 
 class Problem(models.Model):
 
   solution = models.CharField(max_length = 10)
   
   value = models.IntegerField(default = 20)
-  wrong = models.IntegerField(default = 0)
   corect = models.IntegerField(default = 0)
   
   contest = models.ForeignKey(Contest)
+    
+
+  def score_can_change(self):
+    return ( self.corect == 0 and
+             self.contest.duration - self.contest.time_passed().minutes > 20 )
+
+  def score(self):
+    extra_points = self.can_score_change() and self.contest.time_passed().minutes or 0  
+    return self.value + extra_points
+    
+    
 
 class Team(models.Model):
 
-  name = models.CharField(max_length=40)
-  score = models.IntegerField()
+  name = models.CharField(max_length = 40)
+  score = models.IntegerField(default = 120 )
+  special_score = models.IntegerField(default = 0)
   special_problem = models.ForeignKey(Problem)
+
+
 
 class Answer(models.Model):
 
   time = models.DateTimeField()
   team = models.ForeignKey(Team)
-  done = models.BooleanField(default = False)
+  status = models.IntegerField(default = 0)
+  # 0 - Not evaluated
+  # 1 - Evaluated but wrong
+  # 2 - Evaluated and correct
+  # 3 - Evaluated but ignored
   problem = models.ForeignKey(Problem)
   solution = models.CharField(max_length = 10)
+  
+  
   
   
 

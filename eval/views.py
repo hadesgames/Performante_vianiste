475f3d432110325182697979db3e9e7be9d59739
Alexandru Tache <hadesgames@gmail.com>
2011-05-08 01:13:59 +0300

    Added the team view with copy paste from problem view

--- eval/views.py
+++ eval/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render_to_response
 
 def problems(request, contest_id):
   return render_to_response("problems.html")
+
+def teams(request, contest_id):
+  return render_to_response("teams.html")
   

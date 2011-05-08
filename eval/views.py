cf395d9d084f43911d15259c430976b1c212ea30
Alexandru Tache <hadesgames@gmail.com>
2011-05-08 17:20:42 +0300

    Merged all the templates into  base.html. Put the javascript code into separate files.

--- eval/views.py
+++ eval/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render_to_response
 
 def problems(request, contest_id):
-  return render_to_response("problems.html")
+  return render_to_response("problems.html",{"api_address" : "/sever/api/problems/1"})
 
 def teams(request, contest_id):
-  return render_to_response("teams.html")
+  return render_to_response("teams.html", {"api_address" : "/sever/api/teams/1"})
 
 def table(request, contest_id):
-  return render_to_response("table.html");  
+  return render_to_response("table.html",{"api_address" : "/sever/api/table/1"})

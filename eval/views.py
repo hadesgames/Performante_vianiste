1fa6cabbe3def2f1aeda2787c18b56ed609a23f2
Alexandru Tache <hadesgames@gmail.com>
2011-05-08 18:33:36 +0300

    Incomplete version of sendint the remaining time to the js timer

--- eval/views.py
+++ eval/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response
+import models
 
 def problems(request, contest_id):
-  return render_to_response("problems.html",{"api_address" : "/sever/api/problems/1"})
+  return render_to_response("problems.html",{"api_address" : "/sever/api/problems/1",
+      "time_left": models.Contest.objects.get(pk = contest_id).duration * 60 - models.Contest.objects.get(pk = contest_id).time_passed()})
 
 def teams(request, contest_id):
   return render_to_response("teams.html", {"api_address" : "/sever/api/teams/1"})

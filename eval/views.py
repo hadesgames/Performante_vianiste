c747ae0be4a223510ab2135017e1158d73c583de
Alexandru Tache <hadesgames@gmail.com>
2011-05-08 20:24:58 +0300

    The timer now displays the correct value on every page

--- eval/views.py
+++ eval/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render_to_response
 import models
 
+
 def problems(request, contest_id):
   return render_to_response("problems.html",{"api_address" : "/sever/api/problems/1",
-      "time_left": models.Contest.objects.get(pk = contest_id).duration * 60 - models.Contest.objects.get(pk = contest_id).time_passed()})
+      "time_left": models.Contest.objects.get(pk = contest_id).time_remaining()})
 
 def teams(request, contest_id):
-  return render_to_response("teams.html", {"api_address" : "/sever/api/teams/1"})
+  return render_to_response("teams.html", {"api_address" : "/sever/api/teams/1",
+      "time_left": models.Contest.objects.get(pk = contest_id).time_remaining()})
 
 def table(request, contest_id):
-  return render_to_response("table.html",{"api_address" : "/sever/api/table/1"})
+  return render_to_response("table.html",{"api_address" : "/sever/api/table/1",
+      "time_left": models.Contest.objects.get(pk = contest_id).time_remaining()})

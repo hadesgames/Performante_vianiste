0efa7ff91ad6b98e381d5f05468927c70f27682a
Alexandru Tache <hadesgames@gmail.com>
2011-05-10 19:50:30 +0300

    There are no more hard coded links. Closes #2.

--- eval/views.py
+++ eval/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
 import models
 
 
 def problems(request, contest_id):
-  return render_to_response("problems.html",{"api_address" : "/sever/api/problems/%s" % contest_id,
-      "time_left": models.Contest.objects.get(pk = contest_id).time_remaining()})
+  return render_to_response("problems.html",
+      {
+        "api_address" : reverse("eval.api.problems",args=(contest_id,)),
+        "time_left": models.Contest.objects.get(pk = contest_id).time_remaining()
+      }, context_instance = RequestContext(request))
 
 def teams(request, contest_id):
-  return render_to_response("teams.html", {"api_address" : "/sever/api/teams/%s" % contest_id,
-      "time_left": models.Contest.objects.get(pk = contest_id).time_remaining()})
+  return render_to_response("teams.html", 
+      {
+        "api_address" : reverse("eval.api.teams",args = (contest_id,)),
+        "time_left": models.Contest.objects.get(pk = contest_id).time_remaining()
+      }, context_instance = RequestContext(request) )
 
 def table(request, contest_id):
-  return render_to_response("table.html",{"api_address" : "/sever/api/table/%s" % contest_id,
-      "time_left": models.Contest.objects.get(pk = contest_id).time_remaining()})
+  return render_to_response("table.html",
+      {
+        "api_address" : reverse("eval.api.table",args = (contest_id,)),
+        "time_left": models.Contest.objects.get(pk = contest_id).time_remaining()
+      }, context_instance = RequestContext(request))

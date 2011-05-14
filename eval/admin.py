fdfa602895fa775883e31547709329858afec92e
Alexandru Tache <hadesgames@Alexandru-Taches-MacBook-Pro.local>
2011-05-14 17:27:03 +0300

    Added admin interface.

---
+++ eval/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from models import *
+
+admin.site.register(Contest)
+admin.site.register(Problem)
+admin.site.register(Team)
+admin.site.register(Answer)
--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
                        url( r'api/problems/(\d)+', 'eval.api.problems' ),
@@ -17,8 +17,8 @@ urlpatterns = patterns('',
     # url(r'^Performante_vianiste/', include('Performante_vianiste.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+                      #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+     
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+                      url(r'^admin/', include(admin.site.urls)),
 )

7b97374be6e3f2f08fc36449350e05d775510cdf
Alexandru Tache <hadesgames@Alexandru-Taches-MacBook-Pro.local>
2011-05-16 21:55:34 +0300

    Finished the admin interface

--- eval/admin.py
+++ eval/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import *
 
 class ContestAdmin(admin.ModelAdmin):
-  fields = ("start_time","duration")
+  fields = ("start_time", "duration")
   
   
 admin.site.register(Contest, ContestAdmin)
@@ -14,7 +14,14 @@ class ProblemAdmin(admin.ModelAdmin):
 admin.site.register(Problem, ProblemAdmin)
 
 class TeamAdmin(admin.ModelAdmin):
-  fields = ("name","contest")
+  fields = ("name", "contest", "special_problem")
+  
+  def get_readonly_fields(self, request, obj=None):
+    if obj is None:
+        return ()
+    if obj.special_problem is not None:
+        return ("special_problem",)
+    return ()
         
         
 admin.site.register(Team, TeamAdmin)
